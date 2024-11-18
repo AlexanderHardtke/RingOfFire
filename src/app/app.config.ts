43de295f45cef48da022fc85ec317d7832621cca
Alexander Hardtke <5yl3kz@googlemail.com>
2024-11-18 20:52:51 +0100

    keks

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-8165f","appId":"1:802738305174:web:2b6a6bfee4b8c1fadbeab7","storageBucket":"ring-of-fire-8165f.firebasestorage.app","apiKey":"AIzaSyA_ELVJebmoqtZvHh0umqXhVoJOXXSyXtw","authDomain":"ring-of-fire-8165f.firebaseapp.com","messagingSenderId":"802738305174"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
