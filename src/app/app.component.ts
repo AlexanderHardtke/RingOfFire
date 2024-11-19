e26441838da4db9f1422c5619c35098008b09578
Alexander Hardtke <5yl3kz@googlemail.com>
2024-11-19 05:07:52 +0100

    keks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
   title = 'ringOfFire';
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'cards')
+    this.items$ = collectionData(aCollection);
+  }
+
+  getCardsRef() {
+    return collection(this.firestore, 'cards');
+  }
+
+  // getCurrentCardRef() {
+  //   return doc(collection(this.firestore, colId), docId)
+  // }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,5 +9,25 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-8165f","appId":"1:802738305174:web:2b6a6bfee4b8c1fadbeab7","storageBucket":"ring-of-fire-8165f.firebasestorage.app","apiKey":"AIzaSyA_ELVJebmoqtZvHh0umqXhVoJOXXSyXtw","authDomain":"ring-of-fire-8165f.firebaseapp.com","messagingSenderId":"802738305174"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes), provideClientHydration(), provideAnimationsAsync(),
+  provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({
+    "projectId": "ring-of-fire-8165f", "appId": "1:802738305174:web:2b6a6bfee4b8c1fadbeab7",
+    "storageBucket": "ring-of-fire-8165f.firebasestorage.app",
+    "apiKey": "AIzaSyA_ELVJebmoqtZvHh0umqXhVoJOXXSyXtw",
+    "authDomain": "ring-of-fire-8165f.firebaseapp.com",
+    "messagingSenderId": "802738305174"
+  })),
+  provideFirestore(() => getFirestore()),
+  provideDatabase(() => getDatabase()),
+  provideFirebaseApp(() => initializeApp({
+    "projectId": "ring-of-fire-8165f",
+    "appId": "1:802738305174:web:2b6a6bfee4b8c1fadbeab7",
+    "storageBucket": "ring-of-fire-8165f.firebasestorage.app",
+    "apiKey": "AIzaSyA_ELVJebmoqtZvHh0umqXhVoJOXXSyXtw",
+    "authDomain": "ring-of-fire-8165f.firebaseapp.com",
+    "messagingSenderId": "802738305174"
+  })),
+  provideFirestore(() => getFirestore()),
+  provideDatabase(() => getDatabase())]
 };
